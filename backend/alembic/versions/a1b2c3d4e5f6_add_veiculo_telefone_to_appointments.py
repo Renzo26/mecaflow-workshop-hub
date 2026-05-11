529bceee84c2c1672b2a6e332ee34f8f70b58dc6
Renzo26 <arthur.renzo@uscsonline.com.br>
2026-05-11 10:23:37 -0300

    fix: corrigir down_revision da migration de appointments (cadeia quebrada)

--- backend/alembic/versions/a1b2c3d4e5f6_add_veiculo_telefone_to_appointments.py
+++ backend/alembic/versions/a1b2c3d4e5f6_add_veiculo_telefone_to_appointments.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "a1b2c3d4e5f6"
-down_revision: Union[str, None] = "65d080f796d0"
+down_revision: Union[str, None] = "b1c2d3e4f5a6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
